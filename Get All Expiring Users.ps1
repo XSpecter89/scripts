993dfb7892a1b0282b2b34bc443c810c4e3b9e66
Kyle Spencer <kyle.spencer89@live.com>
2019-06-19 08:28:02 -0700

    Modified the script to allow setting how far out you want to query the expiration date.

---
+++ Get All Expiring Users.ps1
@@ -0,0 +1,7 @@
+﻿# Queries AD users for accounts expiring with the specified number of days
+$daysToExpire = 90
+
+Get-ADUser -Filter * -Properties * `
+| Where {($_.accountexpirationdate -ne $null) -and ($_.accountexpirationdate -le ([DateTime]::Now).AddDays($daysToExpire)) -and ($_.enabled -eq 'true')} `
+| Select name, distinguishedName, accountExpirationDate, @{n='manager';e={(Get-ADUser $_.manager).name}} `
+| Export-CSV -Path "\\glk-fs01\shared\it\Scripts\Expired Users.csv" -NoTypeInformation
